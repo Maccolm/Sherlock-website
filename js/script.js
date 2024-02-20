7ac37a58d2a81065ccfe524fe58ed9bfc4c7c7e3
Vitalii Shapovalov <martyit@ukr.net>
2024-02-21 00:49:46 +0100

    changed back

--- js/script.js
+++ js/script.js
@@ -58,10 +58,10 @@ class AdaptiveBlock{
 			this.parentBlockToMove.append(this.blockToMove)
 	}
 }
-// const adBlock = new AdaptiveBlock('.experiense-text__row', '.text__label', '.text__experience', '1200')
+const adBlock = new AdaptiveBlock('.experiense-text__row', '.text__label', '.text__experience', '1200')
 
-// //adaptive block actions move to block-picture
-// const moveActionsBlock = new AdaptiveBlock('.actions', '.media-block__picture','.media-block__actions', '1322')
+//adaptive block actions move to block-picture
+const moveActionsBlock = new AdaptiveBlock('.actions', '.media-block__picture','.media-block__actions', '1322')
 
 //адаптив для слайдера. Робимо один слайдер при ширині екрану
 
